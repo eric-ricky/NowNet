c44ef01fa2702b2e4b91c4b3a6e19993ef285429
ericricky <ericricky200@gmail.com>
2024-09-24 20:39:52 +0300

    under construction

--- app/layout.tsx
+++ app/layout.tsx
@@ -42,7 +42,31 @@ export default function RootLayout({
         />
 
         <ConvexClientProvider>
-          {children}
+          <div>
+            <section className="bg-gray-900 text-white">
+              <div className="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen lg:items-center">
+                <div className="mx-auto max-w-3xl text-center">
+                  <h1 className="bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 bg-clip-text text-3xl font-extrabold text-transparent sm:text-5xl">
+                    We&apos;re Under Development
+                  </h1>
+
+                  <p className="mx-auto mt-4 max-w-xl sm:text-xl/relaxed">
+                    We will be ready soon!
+                  </p>
+
+                  <div className="mt-8 flex flex-wrap justify-center gap-4">
+                    <a
+                      className="block w-full rounded border border-blue-600 bg-blue-600 px-12 py-3 text-sm font-medium text-white hover:bg-transparent hover:text-white focus:outline-none focus:ring active:text-opacity-75 sm:w-auto"
+                      href="mailto:anownet@gmail.com"
+                    >
+                      Get in Touch
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </section>
+            <div className="hidden">{children}</div>
+          </div>
 
           <Toaster />
           <ModalProvider />
