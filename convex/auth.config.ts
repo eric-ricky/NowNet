30370ccb9228985438d704bd62a2d8f806254b60
ericricky <ericricky200@gmail.com>
2024-10-22 14:15:01 +0300

    mvp v0 for convex env to load

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -2,7 +2,6 @@ const config = {
   providers: [
     {
       domain: "https://fair-jawfish-26.clerk.accounts.dev",
-      // domain: "https://clerk.nownet.top",
       applicationID: "convex",
     },
   ],
