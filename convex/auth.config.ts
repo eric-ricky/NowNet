8900e5611ed7d0ab7f2284abdb1b88fa24b3c95e
ericricky <ericricky200@gmail.com>
2024-10-23 19:54:57 +0300

    mvp v0 clerk convex auth config

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,7 +1,8 @@
 const config = {
   providers: [
     {
-      domain: "https://fair-jawfish-26.clerk.accounts.dev",
+      // domain: "https://fair-jawfish-26.clerk.accounts.dev",
+      domain: "https://clerk.nownet.top",
       applicationID: "convex",
     },
   ],
